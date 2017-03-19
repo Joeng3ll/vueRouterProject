317d37da5dd8b8cfdaf213b7deef7924dfe3e8fd
Joeng3ll <272679540@qq.com>
2017-03-19 16:05:48 +0800

    The first study-record about due-router

---
+++ src/components/secondcoponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="secondcomponent">
+      <h1>hello,it's second page</h1>
+      <a>written by {{author}}</a>
+  </div>
+</template>
+<script>
+  export default{
+      data (){
+          return{
+              author:"yanglingli"
+          }
+      }
+  }
+</script>
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+//定义组件
+const first={template:'<div><h1>这是第一个页面</h1></div>'};
+import secondComponent from './components/secondcoponent.vue';
+
+//创建一个路由器实例
+const router=new VueRouter({
+  mode:"history",
+  base:__dirname,
+  routes:[
+    {
+      path:"/first",
+      component:first
+    },
+    {
+      path:"/second",
+      component:secondComponent
+    }
+  ]
+})
+/* eslint-disable no-new */
+new Vue({
+  // template: '<App/>',
+  // components: { App },
+  router:router,
+  render:h=>h(App)
+}).$mount("#app")
